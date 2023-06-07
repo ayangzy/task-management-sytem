6f550f7306c838013ceb22e4813ded6f8c761ecf
felix ayange <felixdecoder2020@gmail.com>
2023-06-07 17:07:18 +0100

    updates

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo systemctl start php-fpm
+sudo systemctl start php8.0-fpm
 sudo systemctl start nginx
--- scripts/application_stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo systemctl stop nginx
-sudo systemctl stop php8.0-fpm
