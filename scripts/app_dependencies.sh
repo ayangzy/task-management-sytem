c920ab3e9614050ec28e058daf8386d2d85e08a2
felix ayange <felixdecoder2020@gmail.com>
2023-06-07 12:51:54 +0100

    update app dependencies scripts

--- scripts/app_dependencies.sh
+++ scripts/app_dependencies.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" 
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" 
 
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 sudo chmod +x /usr/local/bin/composer
 
-cp .env.example .env
-
-composer install --optimize-autoloader
+sudo composer install --optimize-autoloader
